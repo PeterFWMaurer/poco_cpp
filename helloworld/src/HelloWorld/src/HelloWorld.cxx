1bd6b8ccd2b18997bc9176da40c1bcaf42fb4b2f
Peter Maurer <p.maurer@maurer-treutner.de>
2017-01-29 16:59:49 +0100

    first version of hello world

---
+++ helloworld/src/HelloWorld/src/HelloWorld.cxx
@@ -0,0 +1,16 @@
+/*
+ * HelloWorld.cxx
+ *
+ *  Created on: 29.01.2017
+ *      Author: peter
+ */
+
+#include <iostream>
+
+int main()
+{
+	std::cout<<"Hello, World !!!"<<std::endl;
+	return 0;
+}
+
+
