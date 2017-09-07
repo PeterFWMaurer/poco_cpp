d33df7829e2ad5ca03ea422bf905cad94ecc0974
Peter Maurer <p.maurer@maurer-treutner.de>
2017-09-07 08:52:00 +0200

    ...

--- source/EventDemo/source/SimpleEventSample.cpp
+++ source/EventDemo/source/SimpleEventSample.cpp
@@ -12,7 +12,48 @@
 
 namespace EventDemo
 {
-	class SampleSource
+	using Poco::BasicEvent;
+
+	class EventData
+	{
+	public:
+		typedef BasicEvent<EventData> EventDataEvent;
+
+		EventData(int value,const std::string &name):
+			_value(value),
+			_name(name)
+		{}
+
+		virtual ~EventData()
+		{}
+
+		int value()
+		{
+			return _value;
+		}
+
+		const std::string& name()
+		{
+			return _name;
+		}
+	private:
+		int _value;
+		std::string _name;
+	};
+
+	class SampleEventSource
+	{
+	public:
+		void fireEvent(EventData evd)
+		{
+			_event(this,evd);
+			//_event.notify(this,EventData(1,myEvent));
+		}
+	private:
+		EventData::EventDataEvent _event;
+	};
+
+	class SampleEventTarget
 	{
 
 	};
